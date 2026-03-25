b662e6309f24e6d8c4d742592c2fa80a2e46e72f
Xxxi <3544159204@qq.com>
2026-03-25 19:55:18 +0800

    拉取前的提交

---
+++ src/main/java/com/csu/jpetstore/controller/CatalogController.java
@@ -0,0 +1,20 @@
+package com.csu.jpetstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/catalog")
+public class CatalogController {
+    @GetMapping("/main")
+    public String viewMain() {
+        return "catalog/main";
+    }
+    @PostMapping("/main")
+    public String postMain() {
+        return viewMain();
+    }
+
+}
